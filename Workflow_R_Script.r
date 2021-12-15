8d72481abfef5361eeb70aa681ecbb717a9b4111
Mahnoor Zulfiqar <50405440+zmahnoor14@users.noreply.github.com>
2021-12-15 19:09:12 +0100

    added hmdb and gnps function

--- Workflow_R_Script.r
+++ Workflow_R_Script.r
@@ -7,6 +7,8 @@ library(stringr)
 
 input_dir <- paste(getwd(), "/", sep = '')
 
+input_dir
+
 # Load all spectral libraries
 load(file = "/Users/mahnoorzulfiqar/OneDriveUNI/MZML/gnps.rda")
 load(file = "/Users/mahnoorzulfiqar/OneDriveUNI/MZML/hmdb.rda")
@@ -16,6 +18,8 @@ load(file = paste(input_dir, "R_Functions.RData", sep = ''))
 
 input_table <- ms2_rfilename(input_dir)
 
+input_table
+
 x = "/Users/mahnoorzulfiqar/Standards_CodeSet/VN_211016_propanoyl_carnitine.mzML"
 
 precursorMZs <- spec_Processing(x)
@@ -23,6 +27,30 @@ sps_all <- precursorMZs[[1]]
 pre_mz<- precursorMZs[[2]]
 
 a <- precursorMZs[[2]]
+
+df_gnps <- spec_dereplication(a, 
+                              db = "GNPS", 
+                              result_dir = "/Users/mahnoorzulfiqar/Standards_CodeSet/VN_211016_propanoyl_carnitine",
+                             file_id = "file_13",
+                             input_dir)
+
+df_hmdb <- spec_dereplication(a, 
+                              db = "HMDB", 
+                              result_dir = "/Users/mahnoorzulfiqar/Standards_CodeSet/VN_211016_propanoyl_carnitine",
+                             file_id = "file_13",
+                             input_dir)
+
+df_hmdb
+
+df_gnps
+
+merge(df_hmdb, df_gnps)
+
+
+
+
+
+
 
 sps <- spec2_Processing(a, spec = "sps_all", ppmx = NULL)
 
