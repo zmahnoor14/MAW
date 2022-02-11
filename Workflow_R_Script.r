f014515437d5add3051434587c014fde2a830f2f
Mahnoor Zulfiqar <50405440+zmahnoor14@users.noreply.github.com>
2022-02-11 13:55:51 +0100

    until sirius_param

--- Workflow_R_Script.r
+++ Workflow_R_Script.r
@@ -1,59 +1,115 @@
+#!/usr/bin/env Rscript
+
+# ---------- Preparations ----------
+# Load Libraries
 library(Spectra)
 library(MsBackendMgf)
 library(MsBackendHmdb)
 library(MsCoreUtils)
 library(MsBackendMsp)
-library(stringr)
 library(readr)
 library(dplyr)
+# 3 dependencies for latest MassBank version
+library(rvest)
+library(stringr)
+library(xml2)
 
+# ---------- Script ----------
+# input directory
 input_dir <- paste(getwd(), "/", sep = '')
 input_dir
 
-# Load all spectral libraries
-load(file = "/Users/mahnoorzulfiqar/OneDriveUNI/MZML/gnps.rda")
-load(file = "/Users/mahnoorzulfiqar/OneDriveUNI/MZML/hmdb.rda")
-load(file = "/Users/mahnoorzulfiqar/OneDriveUNI/MZML/mbankNIST.rda")
+# load the functions file
+source(file = paste(input_dir, "Worlfow_R_Functions.r", sep = ''))
 
-load(file = paste(input_dir, "R_Functions.RData", sep = ''))
-
-adducts = "/Users/mahnoorzulfiqar/OneDriveUNI/MZML/MetFrag_AdductTypes.csv"
+# load db spectra objects [gnps, hmdb, mbank]
+#download_specDB(input_dir, db = "all")
+#load_specDB(input_dir, db = "gnps") # doesnt work
 
 
 
-input_table <- ms2_rfilename(input_dir)
+# OR load without using function 
+load(file = paste(input_dir,"gnps.rda", sep = ""))
+load(file = paste(input_dir,"hmdb.rda", sep = ""))
+load(file = paste(input_dir,"mbank.rda", sep = ""))
 
+# create result folders and store in a dataframe
+input_table <- data.frame(ms2_rfilename(input_dir))
 input_table
 
-for (i in nrow(input_table)){
+spec_pr <- spec_Processing(as.character(input_table[1, "mzml_files"]))
+sps_all <- spec_pr[[1]]
+pre_mz<- spec_pr[[2]]
+#x <- as.numeric(pre_mz)
+
+df_derep <- spec_dereplication(pre_mz[1], 
+                                       db = "all", 
+                                       result_dir = input_table[1, "ResultFileNames"],
+                                       file_id = input_table[1, "File_id"],
+                                       input_dir,
+                                       ppmx = 15)
+
+spec_pr2 <- ms2_peaks(spec_pr, input_table[1, "ResultFileNames"])
+
+cam_funcMode(path = paste(input_dir, "QC", sep =""), pattern = "common")
+
+merge_qc(path = paste(input_dir, "QC", sep =""))
+
+cam_func(path = "/Users/mahnoorzulfiqar/Project_Scost/QC/", f = "DS200212_Scost_QC_280k_pos.mzML", mode = "pos")
+cam_func(path = "/Users/mahnoorzulfiqar/Project_Scost/QC/", f = "DS200212_Scost_QC_280k_neg.mzML", mode = "neg")
+
+for (i in 1:nrow(input_table)){
+    # if a certain phrase is present in the data files e.g: pos, then take the pos CAMERA
+    if (grepl("SC_full_PRM_pos", input_table[i, "mzml_files"], fixed=TRUE)){
+        input_table[i, "qcCAM_csv"] <- "./QC/Combined_Camera_pos.csv"
+    }
+    if (grepl("SC_full_PRM_neg", input_table[i, "mzml_files"], fixed=TRUE)){
+        input_table[i, "qcCAM_csv"] <- "./QC/Combined_Camera_neg.csv"
+    }
+    if (grepl("DS200309_Scost_QC_70k_pos_PRM.mzML", input_table[i, "mzml_files"], fixed=TRUE)){
+        input_table[i, "qcCAM_csv"] <- "./QC/posCAMERA_Results_DS200212_Scost_QC.csv"
+    }
+    if (grepl("DS200309_Scost_QC_70k_neg_PRM.mzML", input_table[i, "mzml_files"], fixed=TRUE)){
+        input_table[i, "qcCAM_csv"] <- "./QC/negCAMERA_Results_DS200212_Scost_QC.csv"
+    }
+}
+input_table
+
+ms1p <- ms1_peaks(x = spec_pr2, 
+                  y = input_table[1, 'qcCAM_csv'], 
+                  result_dir = input_table[1, 'ResultFileNames'], 
+                  QC = TRUE)
+
+sirius_param_files <- sirius_param(ms1p, 
+                                   result_dir = input_table[1, 'ResultFileNames'], 
+                                   SL = TRUE)
+
+sirius_param_files
+
+
+
+adducts <- paste(input_dir, "MetFrag_AdductTypes.csv", sep = '')
+
+# ---------- multiple files ----------
+
+for (i in 1:nrow(input_table)){
     spec_pr <- spec_Processing(as.character(input_table[i, "mzml_files"]))
     sps_all <- spec_pr[[1]]
     pre_mz<- spec_pr[[2]]
     
     for (a in pre_mz){
         
-        df_mbank <- spec_dereplication(a, 
-                                db = "MassBank", 
-                                result_dir = input_table[i, "ResultFileNames"],
-                                file_id = input_table[i, "File_id"],
-                                input_dir)
-        df_gnps <- spec_dereplication(a, 
-                                db = "GNPS", 
-                                result_dir = input_table[i, "ResultFileNames"],
-                                file_id = input_table[i, "File_id"],
-                                input_dir)
-        df_hmdb <- spec_dereplication(a, 
-                                db = "HMDB", 
-                                result_dir = input_table[i, "ResultFileNames"],
-                                file_id = input_table[i, "File_id"],
-                                input_dir)
+        df_derep <- spec_dereplication(a, 
+                                       db = "all", 
+                                       result_dir = input_table[i, "ResultFileNames"],
+                                       file_id = input_table[i, "File_id"],
+                                       input_dir,
+                                       ppmx = 15)
     }
     
     spec_pr2 <- ms2_peaks(spec_pr, input_table[i, "ResultFileNames"])
     spec_pr3 <- ms1_peaks(spec_pr2, y = NA, result_dir = input_table[i, "ResultFileNames"], QCfile = FALSE)
     sirius_pfiles <- sirius_param(spec_pr3, result_dir = input_table[i, "ResultFileNames"], SL = FALSE)
-    
-    sirius_pfiles <- data.frame(sirius_pfiles)
     
     for (b in nrow(sirius_pfiles)){
         system(paste("sirius --input", sirius_pfiles[b, "sirius_param_file"], 
@@ -67,36 +123,9 @@ for (i in nrow(input_table)){
     met_param <- metfrag_param(sirius_pproc, result_dir = input_table[i, "ResultFileNames"],
                                input_dir, adducts, sl_mtfrag = NA, SL = FALSE)
     for (files in met_param){
-        system(paste("java -jar",  "/Users/mahnoorzulfiqar/MetFrag2.4.5-CL.jar", files))
+        system(paste("java -jar",  paste(input_dir, "MetFrag2.4.5-CL.jar", sep = ''), files))
     }
 }
-
-spec_pr <- spec_Processing(as.character(input_table[1, "mzml_files"]))
-sps_all <- spec_pr[[1]]
-pre_mz<- spec_pr[[2]]
-    
-
-spec_pr2 <- ms2_peaks(spec_pr, input_table[1, "ResultFileNames"])
-spec_pr3 <- ms1_peaks(spec_pr2, y = NA, result_dir = input_table[1, "ResultFileNames"], QCfile = FALSE)
-sirius_pfiles <- sirius_param(spec_pr3, result_dir = input_table[1, "ResultFileNames"], SL = FALSE)
-    
-sirius_pfiles <- data.frame(sirius_pfiles)
-for (b in nrow(sirius_pfiles)){
-    system(paste("sirius --input", sirius_pfiles[b, "sirius_param_file"], 
-                    "--output", sirius_pfiles[b, "outputNames"],
-                "formula --profile orbitrap --no-isotope-filter --no-isotope-score --candidates 30 --ppm-max 5 --ppm-max-ms2 15 structure --database ALL canopus", 
-                    sep = " "))
-}
-    
-sirius_pproc <- sirius_postprocess(input_table[1, "ResultFileNames"], SL = FALSE)
-    
-met_param <- metfrag_param(sirius_pproc, result_dir = input_table[1, "ResultFileNames"],
-                            input_dir, adducts, sl_mtfrag = NA, SL = FALSE)
-for (files in met_param){
-    system(paste("java -jar",  "/Users/mahnoorzulfiqar/MetFrag2.4.5-CL.jar", files))
-}
-
-#system('java -jar /Users/mahnoorzulfiqar/MetFrag2.4.5-CL.jar /Users/mahnoorzulfiqar/Standards_CodeSet/VN_211016_acetyl_carnitine/insilico/MetFrag/1_id_M204R149ID1_mz_204.122756958008_rt_148.997391_db_PubChem.txt')
 
 
 
