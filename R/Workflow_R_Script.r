03a873c56178cc5f6e91987dd55c6dad03d1836b
Mahnoor Zulfiqar <zmahnoor14@gmail.com>
2022-08-24 00:54:08 +0200

    new dockerfile

--- R/Workflow_R_Script.r
+++ R/Workflow_R_Script.r
@@ -1,134 +1,73 @@
-#packageVersion("Spectra")
-#packageVersion("MsBackendMgf")
-#packageVersion("MsBackendHmdb")
-#packageVersion("MsBackendMsp")
-#packageVersion("MsCoreUtils")
-#packageVersion("readr")
-#packageVersion("dplyr")
-#packageVersion("rvest")
-#packageVersion("stringr")
-#packageVersion("xml2")
-#packageVersion("CAMERA")
-
-
+start.time <- Sys.time()
 
 # ---------- Script ----------
 # input directory
-input_dir <- getwd()
+input_dir <- paste(getwd(), "/data", sep = "")
 input_dir
 
 # load the functions file
-source(paste(input_dir, "/Workflow_R_Functions.r", sep = ""))
+source(paste(getwd(), "/Workflow_R_Functions.r", sep = ""))
 
-# Track Time 
-#start_time <- Sys.time()
+input_table <- data.frame(ms2_rfilename(input_dir))
+input_table
 
-line <- system.time({
-    # Run the first function; this creates a dataframe of your input files, their result directories 
-    # and gives an id to each input file; stores the table in directory as a csv filr
-    input_table <- data.frame(ms2_rfilename(input_dir))
-    input_table
-    for (i in 1:nrow(input_table)){
-
-        #Preprocess and Read the mzMLfiles
-        spec_pr <- spec_Processing(input_dir,
-                                   input_table[i, "mzml_files"], 
-                                   input_table[i, "ResultFileNames"])
-
-
-        #perform dereplication with all dbs
-        df_derep <- spec_dereplication(mzml_file = input_table[i, "mzml_files"],
-                                       pre_tbl = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/premz_list.txt", sep = ""), "."), sep =""), 
-                                       proc_mzml = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/processedSpectra.mzML", sep = ""), "."), sep =""),
-                                       db = "all", 
-                                       result_dir = input_table[i, "ResultFileNames"],
-                                       file_id = input_table[i, "File_id"], 
-                                       input_dir, 
-                                       ppmx = 15)
-
-        # Extract MS2 peak lists
-        spec_pr2 <- ms2_peaks(pre_tbl = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/premz_list.txt", sep = ""), "."), sep =""), 
-                              proc_mzml = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/processedSpectra.mzML", sep = ""), "."), sep =""),
-                              input_dir,
-                              input_table[i, "ResultFileNames"],
-                             file_id = input_table[i, "File_id"])
-
-        # camera results for isotopes
-        cam_res <- cam_func(input_dir,
-                            f = input_table[i, "mzml_files"], 
-                            ms2features = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/insilico/MS2DATA.csv", sep = ""), "."), sep = ""))
-
-
-        # Extract MS1 peaks or isotopic peaks
-        ms1p <- ms1_peaks(x = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"],'/insilico/MS2DATA.csv', sep = ""), "."), sep =""), 
-                          y = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"],'/CAMERAResults.csv', sep = ""), "."), sep =""), 
-                          input_table[i, "ResultFileNames"], 
-                          input_dir, 
-                          QCfile = TRUE)
-        #prepare sirius parameter files
-        sirius_param_files <- sirius_param(x = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"],'/insilico/MS1DATA.csv', sep = ""), "."), sep =""), 
-                                           result_dir = input_table[i, 'ResultFileNames'], 
-                                           input_dir,
-<<<<<<< HEAD
-                                           SL = FALSE)
-=======
-                                           SL = TRUE)
->>>>>>> refs/remotes/origin/main
-
-        # Run sirius
-        run_sirius(files = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"],'/insilico/MS1DATA_SiriusPandSL.csv', sep = ""), "."), sep =""), 
-                   ppm_max = 5, 
-                   ppm_max_ms2 = 15, 
-                   QC = TRUE, 
-<<<<<<< HEAD
-                   SL = FALSE, 
-                   SL_path = NA,
-=======
-                   SL = TRUE, 
-                   SL_path = paste(input_dir, 'SL_Frag/', sep = ""),
->>>>>>> refs/remotes/origin/main
-                   candidates = 30)
-
-
-        # Post process Sirius results and extract adducts for MetFrag
-        adducts <- sirius_adduct(input_dir,
-                                 x = input_table[i, "ResultFileNames"], 
-<<<<<<< HEAD
-                                 SL = FALSE)
-=======
-                                 SL = TRUE)
->>>>>>> refs/remotes/origin/main
-
-
-        # prepare Metfrag parameter files
-        met_param <- metfrag_param(x = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/insilico/MS1DATAsirius.csv", sep = ""), "."), sep =""), 
+for (i in 1:nrow(input_table)){
+    
+    #Preprocess and Read the mzMLfiles
+    spec_pr <- spec_Processing(input_dir,
+                               input_table[i, "mzml_files"], 
+                               input_table[i, "ResultFileNames"])
+    
+    #perform dereplication with all dbs
+    df_derep <- spec_dereplication_file(mzml_file = input_table[i, "mzml_files"],
+                                   pre_tbl = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/premz_list.txt", sep = ""), "."), sep =""), 
+                                   proc_mzml = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/processedSpectra.mzML", sep = ""), "."), sep =""),
+                                   db = "all", 
                                    result_dir = input_table[i, "ResultFileNames"],
+                                   file_id = input_table[i, "File_id"], 
                                    input_dir, 
-<<<<<<< HEAD
-                                   sl_mtfrag = NA, 
-                                   SL = FALSE,
-=======
-                                   sl_mtfrag = paste(input_dir, "/SL_metfrag.txt", sep = ""), 
-                                   SL = TRUE,
->>>>>>> refs/remotes/origin/main
-                                   ppm_max = 5, 
-                                   ppm_max_ms2= 15)
+                                   no_of_candidates = 30,
+                                   ppmx = 15)
+    
+    
+    # Extract MS2 peak lists
+    spec_pr2 <- ms2_peaks(pre_tbl = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/premz_list.txt", sep = ""), "."), sep =""), 
+                          proc_mzml = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/processedSpectra.mzML", sep = ""), "."), sep =""),
+                          input_dir,
+                          result_dir = input_table[i, "ResultFileNames"],
+                         file_id = input_table[i, "File_id"])
+    
+    # camera results for isotopes
+    cam_res <- cam_func(input_dir,
+                        f = input_table[i, "mzml_files"], 
+                        ms2features = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/insilico/MS2DATA.csv", sep = ""), "."), sep = ""))
+    
+    # Extract MS1 peaks or isotopic peaks
+    ms1p <- ms1_peaks(x = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"],'/insilico/MS2DATA.csv', sep = ""), "."), sep =""), 
+                      y = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"],'/CAMERAResults.csv', sep = ""), "."), sep =""), 
+                      input_table[i, "ResultFileNames"], 
+                      input_dir, 
+                      QCfile = TRUE)
+    
+    #prepare sirius parameter files
+    sirius_param_files <- sirius_param(x = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"],'/insilico/MS1DATA.csv', sep = ""), "."), sep =""), 
+                                       result_dir = input_table[i, 'ResultFileNames'], 
+                                       input_dir,
+                                       SL = FALSE)
+    
+    
+    # Run sirius
+    run_sirius(files = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"],'/insilico/MS1DATA_SiriusP.tsv', sep = ""), "."), sep =""), 
+               ppm_max = 5, 
+               ppm_max_ms2 = 15, 
+               QC = TRUE, 
+               SL = FALSE, 
+               SL_path = NA,
+               candidates = 30,
+              profile = "qtof")
+}
 
+end.time <- Sys.time()
 
-        # run metfrag
-        run_metfrag(met_param = paste(input_dir, str_remove(paste(input_table[i, "ResultFileNames"], "/insilico/metparam_list.txt", sep = ""), "."), sep =""))
-
-
-    }
-})
-
-
-write(paste("MAW took", line[1] "of seconds.", sep = "") ,file=paste(input_dir, "/summaryFile.txt", sep = ""),append=TRUE)
-
-<<<<<<< HEAD
-save.image(file = paste(input, "/StandardsRun1.Rdata", sep ="")
-=======
-save.image(file = paste(input, "/BryophytesRun1.Rdata", sep ="")
->>>>>>> refs/remotes/origin/main
-
-
+time.taken <- end.time - start.time
+print(time.taken)
