010eba788369b50761dafdf091f1e16a43b39264
Kohulan <kohulan.rajan@uni-jena.de>
2022-08-31 15:33:51 +0200

    Workflow functions formatted

--- Python/Workflow_Python_Script.py
+++ Python/Workflow_Python_Script.py
@@ -11,15 +11,15 @@ from Workflow_Python_Functions import *
 # In[ ]:
 
 
-#Define input directory, keep all files in same directory and scripts so getwd works
-input_dir = os.getwd()+"/data"
+# Define input directory, keep all files in same directory and scripts so getwd works
+input_dir = os.getwd() + "/data"
 input_dir
 
 
 # In[ ]:
 
 
-spec_postproc(input_dir, Source = "all")
+spec_postproc(input_dir, Source="all")
 
 
 # In[ ]:
@@ -31,13 +31,13 @@ MCSS_for_SpecDB(input_dir, Source)
 # In[ ]:
 
 
-sirius_postproc(input_dir, exp_int = 0.90, csi_score = -150)
+sirius_postproc(input_dir, exp_int=0.90, csi_score=-150)
 
 
 # In[ ]:
 
 
-MCSS_for_SIRIUS(input_dir)    
+MCSS_for_SIRIUS(input_dir)
 
 
 # In[ ]:
@@ -74,4 +74,3 @@ chemMN(input_dir, resultcsv)
 
 
 gnpsMNvsgnpsMAW(input_dir)
-
