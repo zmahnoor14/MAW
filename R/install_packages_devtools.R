ec37883551c72bac6cae0fb48eeb39bc1a8a7669
Mahnoor Zulfiqar <zmahnoor14@gmail.com>
2022-12-13 13:48:27 +0100

    new install files

---
+++ R/install_packages_devtools.R
@@ -0,0 +1,6 @@
+
+install.packages("devtools", repos="http://cloud.r-project.org")
+devtools::install_github("End-to-end-provenance/provSummarizeR")
+devtools::install_github("End-to-end-provenance/rdt")
+devtools::install_github("End-to-end-provenance/rdtLite")
+
