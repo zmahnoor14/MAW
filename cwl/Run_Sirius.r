b56dbd034a397506dcdf732ff57a5dccb18c27c1
Mahnoor Zulfiqar <zmahnoor14@gmail.com>
2023-06-03 21:33:10 +0200

    Run sirius5

--- cwl/Run_Sirius.r
+++ cwl/Run_Sirius.r
@@ -1,7 +1,7 @@
 # provenance library rdtLite integration
-library(rdtLite)
-options(prov.dir = "./prov_metfrag", snapshot.size = 10000)
-prov.init(prov.dir = ".")
+# library(rdtLite)
+# options(prov.dir = "./prov_metfrag", snapshot.size = 10000)
+# prov.init(prov.dir = ".")
 
 #define arguments
 args = commandArgs(trailingOnly = TRUE)
@@ -23,49 +23,58 @@ run_sirius <- function(files, ppm_max = 5, ppm_max_ms2 = 15, QC, SL , SL_path, c
 
         if (QC){
             if (is.na(files[b, "isotopes"])){
-                system(paste("sirius --input", files[b, "sirius_param_file"], "--output", files[b, "outputNames"],
-                             "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database", db ,"canopus",
+                system(paste("sirius --input", files[b, "sirius_param_file"],
+                             "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database", db ,
+                             "canopus write-summaries --output", files[b, "outputNames"],
                              sep = " "))
                 if(!file.exists(files[b, "outputNames"])){
-                    system(paste("sirius --input", files[b, "sirius_param_file"], "--output", files[b, "outputNames"],
-                                 "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database", db ,"canopus",
+                    system(paste("sirius --input", files[b, "sirius_param_file"],
+                                 "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database", db ,
+                                 "canopus write-summaries --output", files[b, "outputNames"],
                                  sep = " "))
                 }
                 if(SL){
-                    system(paste("sirius --input", files[b, "sirius_param_file"], "--output", files[b, "outputNamesSL"],
-                             "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database",  SL_path ,"canopus",
+                    system(paste("sirius --input", files[b, "sirius_param_file"],
+                             "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database",  SL_path ,
+                             "canopus write-summaries --output", files[b, "outputNamesSL"],
                              sep = " "))
                 }
 
             }
             else if(files[b, "isotopes"] == "present"){
-                system(paste("sirius --input", files[b, "sirius_param_file"], "--output", files[b, "outputNames"],
-                             "formula --profile", profile, "--candidates", candidates, "--ppm-max", ppm_max,"--ppm-max-ms2", ppm_max_ms2,"fingerprint structure --database", db ,"canopus",
+                system(paste("sirius --input", files[b, "sirius_param_file"],
+                             "formula --profile", profile, "--candidates", candidates, "--ppm-max", ppm_max,"--ppm-max-ms2", ppm_max_ms2,"fingerprint structure --database", db ,
+                             "canopus write-summaries --output", files[b, "outputNames"],
                              sep = " "))
                 if(!file.exists(files[b, "outputNames"])){
-                     system(paste("sirius --input", files[b, "sirius_param_file"], "--output", files[b, "outputNames"],
-                                 "formula --profile", profile, "--candidates", candidates, "--ppm-max", ppm_max,"--ppm-max-ms2", ppm_max_ms2,"fingerprint structure --database", db ,"canopus",
+                     system(paste("sirius --input", files[b, "sirius_param_file"],
+                                 "formula --profile", profile, "--candidates", candidates, "--ppm-max", ppm_max,"--ppm-max-ms2", ppm_max_ms2,"fingerprint structure --database", db ,
+                                 "canopus write-summaries --output", files[b, "outputNames"],
                                  sep = " "))
                 }
                 if(SL){
-                    system(paste("sirius --input", files[b, "sirius_param_file"], "--output", files[b, "outputNamesSL"],
-                             "formula --profile", profile, "--candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database",  SL_path ,"canopus",
+                    system(paste("sirius --input", files[b, "sirius_param_file"],
+                             "formula --profile", profile, "--candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database",  SL_path ,
+                             "canopus write-summaries --output", files[b, "outputNamesSL"],
                              sep = " "))
                 }
             }
         }
         else{
-            system(paste("sirius --input", files[b, "sirius_param_file"], "--output", files[b, "outputNames"],
-                        "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database", db ,"canopus",
+            system(paste("sirius --input", files[b, "sirius_param_file"], 
+                        "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database", db ,
+                        "canopus write-summaries --output", files[b, "outputNames"],
                         sep = " "))
             if(!file.exists(files[b, "outputNames"])){
-                system(paste("sirius --input", files[b, "sirius_param_file"], "--output", files[b, "outputNames"],
-                             "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database", db ,"canopus",
+                system(paste("sirius --input", files[b, "sirius_param_file"],
+                             "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database", db ,
+                             "canopus write-summaries --output", files[b, "outputNames"],
                              sep = " "))
             }
             if(SL){
-                system(paste("sirius --input", files[b, "sirius_param_file"], "--output", files[b, "outputNamesSL"],
-                            "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database",  SL_path ,"canopus",
+                system(paste("sirius --input", files[b, "sirius_param_file"],
+                            "formula --profile", profile, "--no-isotope-filter --no-isotope-score --candidates", candidates, "--ppm-max", ppm_max, "--ppm-max-ms2",  ppm_max_ms2,"fingerprint structure --database",  SL_path ,
+                            "canopus write-summaries --output", files[b, "outputNamesSL"],
                             sep = " "))
             }
         }
@@ -83,5 +92,5 @@ end.time <- Sys.time()
 # Time taken to run the analysis for MAW-R
 time.taken <- end.time - start.time
 print(time.taken)
-prov.save()
-prov.quit()
+# prov.save()
+# prov.quit()
